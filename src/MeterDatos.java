0218f0d9295569b0a0a9e3f51cda650d162e3443
Jorge Rodriguez Hernandez <78152047+Jorgerodriguezhernandez@users.noreply.github.com>
2021-09-28 10:40:53 +0100

    Feature: Iniciamos el proyecto

---
+++ src/MeterDatos.java
@@ -0,0 +1,8 @@
+
+
+    https://github.com/Jorgerodriguezhernandez/practica1-1
+    public static void main(String[] args) throws IOException {
+        muestraContenido("E:/PGL/Practica1-1/Nombres.csv");
+    }
+
+}
