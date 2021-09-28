3eec65b2104ae4fe99e2b18bd1b0c6bbec8c885a
Jorge Rodriguez Hernandez <78152047+Jorgerodriguezhernandez@users.noreply.github.com>
2021-09-28 10:52:09 +0100

    Se añade un lector FileReader

--- src/MeterDatos.java
+++ src/MeterDatos.java
@@ -1,6 +1,24 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class MeterDatos {
 
-    https://github.com/Jorgerodriguezhernandez/practica1-1
+    public static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
+        ArrayList<Double> Notas = new ArrayList<>();
+        ArrayList<String> Nombres = new ArrayList<>();
+        ArrayList<String> NombresRandom = new ArrayList<>();
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+        while ((b.readLine()) != null) {
+            Nombres.add((b.readLine()));
+
+        }
+    }
     public static void main(String[] args) throws IOException {
         muestraContenido("E:/PGL/Practica1-1/Nombres.csv");
     }
